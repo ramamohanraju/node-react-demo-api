ec653077e2240a5ecaeca4418575daf9318c4a1a
Gadiraju Ramamohan Raju <graju@intralinks.com>
2020-04-10 12:19:48 +0530

    update testAPI text

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res, next) {
-    res.send("API is working properly");
+    res.send("API is working properly. Project added to git.");
 });
 
 module.exports = router;
